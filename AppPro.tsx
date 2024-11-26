bbc6f7e2e8190c3d3c0bdbeb8dc74a5c9d94bb5c
imfarhat <imfarhat.dev@gmail.com>
2024-11-26 22:22:22 +0530

    re-qrote new file for app and uses app rpo imstead of app

---
+++ AppPro.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, SafeAreaView} from 'react-native';
+
+function AppPro() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Text From AppPro</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default AppPro;
