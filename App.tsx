240d0f931cd0c6b08fe76e5d7e4344d1a90705aa
imfarhat <imfarhat.dev@gmail.com>
2024-12-07 21:53:11 +0530

    installed nativewind and setup of the same

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import FlatCard from './components/FlatCards';
 import ElevatedCards from './components/ElevatedCards';
 import FancyCards from './components/FancyCards';
 import ActionCard from './components/ActionCard';
+import './global.css';
 
 const App = () => {
   return (
